3dbbb2351cf6d32c2d3297c176976314012049e0
elephmoon <alexandr.kizilov@pm.me>
2024-02-03 10:02:37 +0300

    add simple api

--- project.clj
+++ project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns clojure-server.core})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 ]
+  :repl-options {:init-ns clojure-server.core}
+  :main clojure-server.core)
--- src/clojure_server/core.clj
+++ src/clojure_server/core.clj
@@ -1,6 +1,22 @@
-(ns clojure-server.core)
+(ns clojure-server.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn app [request]
+  (let [{:keys [uri request-method]} request]
+    {:status 200
+     :headers {"Content-Type" "text/plain"}
+     :body (format "You requested %s %s" (-> request-method name .toUpperCase) uri)}))
+
+(app {:request-method :get :uri "/index.html"})
+{:status 200
+ :headers {"Content-Type" "text/plain"}
+ :body "GET /index.html"}
+
+(app {:request-method :post :uri "/user"})
+{:status 200
+ :headers {"Content-Type" "text/plain"}
+ :body "POST /user"}
+
+(defn -main
+  [& args]
+  (run-jetty app {:port 8888 :join? true}))
